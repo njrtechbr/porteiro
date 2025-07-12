88d85ca6d7266db0e5cb57aa4d330dd6495415b2
Nereu Jr <nnvljr86@gmail.com>
2025-07-12 22:05:25 +0000

    o pwa ta ativado no access para instalar no android e ios?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
@@ -8,6 +8,10 @@ export const metadata: Metadata = {
   description: 'Sistema inteligente de controle de acesso.',
   manifest: '/manifest.json',
 };
+
+export const viewport: Viewport = {
+  themeColor: '#4A6C4D',
+}
 
 export default function RootLayout({
   children,
@@ -21,6 +25,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet"></link>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Porteiro" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
       <body className={cn('font-body antialiased')}>
         {children}
